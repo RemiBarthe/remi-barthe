a5776ef918912ca159da932ead8c35b70f3beba0
RemiBarthe <barthe.rem@gmail.com>
2022-06-23 23:20:11 +0200

    WIP: add icons and title

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,66 @@
 <template>
-  <div>
-    <StickyButton style="margin-left: 100px; margin-top: 250px"
-      >Ma Value</StickyButton
-    >
+  <div class="page-container">
+    <div class="page-header">
+      <h1 class="name-title">Rémi Barthe</h1>
 
-    <StickyButton>Ma super super Value</StickyButton>
-    <img
-      alt="ok"
-      src="https://dennissnellenberg.com/media/pages/work/emble-studio/cb063dfbe5-1646837261/thumbnail-emble-yellow.jpg"
-    />
+      <div class="links">
+        <StickyButton>
+          <img alt="logo github" src="../assets/images/github.svg" />
+        </StickyButton>
 
-    <div class="pointers">
+        <StickyButton>
+          <img alt="logo at mail" src="../assets/images/at.svg" />
+        </StickyButton>
+
+        <StickyButton>
+          <img alt="logo linkedin" src="../assets/images/linkedin.svg" />
+        </StickyButton>
+      </div>
+    </div>
+
+    <div class="presentation">
+      <div class="title">
+        <div class="title-decoration"></div>
+        <h2>
+          web_ <br />
+          developer
+        </h2>
+      </div>
+      <h3 class="subtitle">Lorem ipsum sit amet dolor loprem alork</h3>
+    </div>
+
+    <!-- <div class="pointers">
       <div
         v-for="pointer in pointers"
         v-show="showPointer"
         class="pointer"
         :style="[pointerPosition, pointerCss(pointer)]"
       ></div>
+
+      <BackgroundForm />
     </div>
+
+    <svg>
+      <defs>
+        <filter id="filter">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+          <feColorMatrix
+            in="blur"
+            mode="matrix"
+            values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -10"
+            result="filter"
+          />
+          <feComposite in="SourceGraphic" in2="filter" operator="atop" />
+        </filter>
+      </defs>
+    </svg> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { default as POINTERS, Pointer } from '@/plugins/pointers';
+import '@/assets/style/global.scss';
 
 export default defineComponent({
   name: 'HomePage',
@@ -58,7 +95,7 @@ export default defineComponent({
       return `
       width: ${pointer.size}px;
       height: ${pointer.size}px;
-      transition-duration: ${pointer.transitionDuration}s;
+      transition-duration: ${pointer.transitionDuration * 2}s;
       margin-top: ${pointer.margin}px;
       margin-left: ${pointer.margin}px;
       `;
@@ -66,24 +103,77 @@ export default defineComponent({
     hidePointer() {
       setTimeout(() => {
         this.showPointer = false;
-      }, 150);
+      }, 250);
     }
   }
 });
 </script>
 
 <style lang="scss" scoped>
-.pointers {
-  .pointer {
+.page-container {
+  padding: 20px 80px;
+  height: calc(100vh - 40px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  .page-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .name-title {
+      font-size: 1.5rem;
+      font-family: 'ZenKakuBlack';
+    }
+    .links {
+      display: flex;
+      transform: rotate(5.45deg);
+      cursor: pointer;
+    }
+  }
+  .presentation {
+    display: flex;
+    flex-flow: column wrap;
+    align-content: center;
+    align-items: flex-end;
+    .title {
+      font-size: 6rem;
+      font-family: 'LeagueSpartan';
+      display: flex;
+
+      h2 {
+        margin: 0;
+      }
+    }
+    .title-decoration {
+      width: 30px;
+      height: 140px;
+      background-color: #4062bb;
+      margin-right: 40px;
+    }
+    .subtitle {
+      font-size: 1.5rem;
+      font-family: 'ZenKakuBold';
+      text-align: right;
+    }
+  }
+  .pointers {
+    width: 100%;
+    height: 100%;
     position: absolute;
     top: 0;
     left: 0;
-    pointer-events: none;
-    will-change: transform;
-    border-radius: 50px;
-    backface-visibility: hidden;
-    background-color: #0a73ba;
-    transition: transform cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    filter: url('#filter');
+    .pointer {
+      position: absolute;
+      top: 0;
+      left: 0;
+      pointer-events: none;
+      will-change: transform;
+      border-radius: 50px;
+      backface-visibility: hidden;
+      background-color: #4062bb;
+      transition: transform cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
   }
 }
 </style>
--- plugins/pointers.ts
+++ plugins/pointers.ts
@@ -6,39 +6,39 @@ export interface Pointer {
 
 export default [
     {
-      size: 18,
-      transitionDuration: 0.34,
+      size: 30,
+      transitionDuration: 0.22,
       margin: 0
     } as Pointer,
     {
-      size: 17,
-      transitionDuration: 0.32,
-      margin: 0.5
-    } as Pointer,
-    {
-      size: 16,
-      transitionDuration: 0.3,
-      margin: 1
+      size: 20,
+      transitionDuration: 0.24,
+      margin: 5
     } as Pointer,
     {
       size: 15,
-      transitionDuration: 0.28,
-      margin: 1.5
+      transitionDuration: 0.26,
+      margin: 7.5
     } as Pointer,
     {
       size: 14,
-      transitionDuration: 0.26,
-      margin: 2
+      transitionDuration: 0.28,
+      margin: 8
     } as Pointer,
     {
       size: 13,
-      transitionDuration: 0.24,
-      margin: 2.5
+      transitionDuration: 0.3,
+      margin: 8.5
     } as Pointer,
     {
-      size: 10,
-      transitionDuration: 0.22,
-      margin: 4
+      size: 12,
+      transitionDuration: 0.32,
+      margin: 9
+    } as Pointer,
+    {
+      size: 11,
+      transitionDuration: 0.34,
+      margin: 9.5
     } as Pointer
   ]
   
